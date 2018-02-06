3fb29246a1db7887c738137e350a1e8fea0a4748
Ivan Sočo <isoco16@gmail.com>
2018-02-06 19:29:29 +0100

    3.

--- XML/XML/XML.cpp
+++ XML/XML/XML.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
 
+using namespace std;
 
+bool Detect(char *firstSign) {
+	if (*firstSign == '<')
+		return true;
+	else
+		return false;
+}
+void Scan_line() {
+	string line;
+	int k;
+	getline(cin, line);
+	k = line.length();
+	if (Detect(&line[0]))
+		cout << "Prvi znak je <" << endl;
+	else
+		cout << "priv znak nije <" << endl;
+
+}
 int main()
 {
+	Scan_line();
     return 0;
 }
 
@@ -13,3 +34,5 @@ int main()
 //call function to scan everything from < to >
 //function that sort in list every tag 
 //function that contains all value names and everything that have =
+
+
