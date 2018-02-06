affdf0930fbdbaf405684b2ddda24f40f67c91f2
Ivan Sočo <isoco16@gmail.com>
2018-02-06 16:56:26 +0100

    dodani komentari

--- XML/XML/XML.cpp
+++ XML/XML/XML.cpp
@@ -9,3 +9,7 @@ int main()
     return 0;
 }
 
+//make function for detecting first sign is < 
+//call function to scan everything from < to >
+//function that sort in list every tag 
+//function that contains all value names and everything that have =
